019f6eec026afd81354a8adece2d7ca5748098cf
Vladimir Abyzov <abizov-v5@rambler.ru>
2016-12-19 12:00:18 +0300

    First commit

---
+++ WebServiceFarmoc/WebServiceFarmoc/Controllers/BasicController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebServiceFarmoc.Controllers
+{
+    public class BasicController : Controller
+    {
+        //
+        // GET: /Basic/
+        public void Execute(RequestContext requestContext)
+        {
+            string controller = (string)requestContext.RouteData.Values["controller"];
+            string action = (string)requestContext.RouteData.Values["action"];
+
+            requestContext.HttpContext.Response.Write(
+                    string.Format("Контроллер: {0}, Метод действия: {1}",
+                    controller, action));
+        }
+
+    }
+}
---
+++ WebServiceFarmoc/WebServiceFarmoc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebServiceFarmoc.WebApiApplication" Language="C#" %>
